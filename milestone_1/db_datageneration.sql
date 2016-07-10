52a86807b42b05107b29c74385430ffbe4c28bd8
clm133 <charliemietzner@gmail.com>
2016-07-10 16:10:30 -0400

    added random generation of groups and group members, figured out what was wrong with belongs_to check constraint in db_creation.sql --I can't call user-defined functions from a check

--- milestone_1/db_datageneration.sql
+++ milestone_1/db_datageneration.sql
@@ -1,26 +1,43 @@
 DECLARE 
+	--User Vars
 	fname varchar2(32);
-  mname varchar2(32);
-  lname varchar2(32);
+	mname varchar2(32);
+	lname varchar2(32);
 	email varchar2(64);
 	DOB date;
 	loggedIn timestamp;
-  v_friend1 INTEGER;
-  v_friend2 INTEGER;
-  temp INTEGER;
-  not_enough_rels BOOLEAN := TRUE;
-  num_rels INTEGER := 0;
+	
+	--Friend Vars
+	v_friend1 INTEGER;
+	v_friend2 INTEGER;
+	temp INTEGER;
+	not_enough_rels BOOLEAN := TRUE;
+	num_rels INTEGER := 0;
+	
+	--Group Vars
+	gname varchar2(32);
+	ml number;
+	gdesc varchar2(100);
+	
+	--Belongs_To VARSAMP
+	belongsID number;
+	belongsML INTEGER;
+	belongsExists BOOLEAN;
+	
+	
 BEGIN
 
   -- DELETE PREVIOUS ROWS
-  DELETE FROM USERS;
+  DELETE FROM Belongs_To;
   DELETE FROM FRIENDSHIPS;
+  DELETE FROM USERS;
+  DELETE FROM Groups;
 
   -- GENERATE RANDOM USERS
 	FOR i in 1..100 LOOP
 		fname := 'first' || i;
-    mname := 'middle' || i;
-    lname := 'last' || i;
+		mname := 'middle' || i;
+		lname := 'last' || i;
 		email := fname || lname || '@pitt.edu';
 		SELECT TO_DATE(TRUNC(DBMS_RANDOM.VALUE(TO_CHAR(DATE '2015-01-01','J'),TO_CHAR(DATE '2016-6-30','J'))),'J') 
                    INTO loggedIn FROM DUAL;
@@ -29,6 +46,7 @@ BEGIN
 		INSERT INTO USERS VALUES(i, fname, mname, lname, email, SYSDATE, SYSDATE);
 	END LOOP;
   commit;
+  
   -- GENREATE RANDOM RELATIONSHIPS
   
   WHILE not_enough_rels LOOP
@@ -63,6 +81,40 @@ BEGIN
   END LOOP;
   
   commit;
+  
+  -- GENREATE RANDOM GROUPS
+  FOR i in 1..10 LOOP
+	gdesc := 'This is a group. its number is ';
+	gname := 'group' || i;
+	gdesc := gdesc || i || '. group number ' || i || ' is the best!';
+	SELECT FLOOR(DBMS_RANDOM.VALUE(1, 100)) INTO ml FROM DUAL; -- I kept the random number generator at 1-100 because I really like that there's a chance there will be a 1 person group and that makes me laugh
+	INSERT INTO Groups VALUES(i, gname, ml, gdesc);
+  END LOOP;
+  commit;
+  
+  -- GENERATE RANDOM BELONGS_TO
+  not_enough_rels := TRUE;
+  num_rels := 0;
+  
+  FOR i in 1..10 LOOP --for each group id (1-10)
+	SELECT memLimit INTO belongsML FROM Groups WHERE groupID = i;
+	WHILE not_enough_rels LOOP
+		belongsExists := TRUE;
+		WHILE belongsExists LOOP
+			SELECT FLOOR(DBMS_RANDOM.VALUE(1, 100)) INTO belongsID FROM DUAL; --generate random userID
+			SELECT COUNT(*) INTO temp FROM Belongs_To WHERE member = belongsID AND groupID = i; -- check if there exists a relation like this already generated
+			belongsExists := temp = 1; --if a row in the belongs_to table already doesn't already exist, this will be a 0, creating a false and breaking the loop
+		END LOOP;
+		INSERT INTO Belongs_To VALUES(belongsID, i);
+		commit;
+		SELECT COUNT(*) INTO num_rels FROM Belongs_To WHERE groupID = i;
+		IF num_rels = belongsML OR num_rels >= 15 THEN --if we've met group capactiy or got at least 15 members in a group, exit loop
+		  not_enough_rels := FALSE;
+		END IF;
+	END LOOP;
+  END LOOP;
+  
+  
   END;
 /
 
